35e95983e7211f9eebe30b6dff97cf9e801c071b
eedloo <mohsen.aghazadeeedloo@slu.edu>
2023-02-18 15:07:57 -0600

    final edit

--- project1/mrf.py
+++ project1/mrf.py
@@ -5,27 +5,14 @@ import json
 import pandas as pd
 import parser
 
-__author__ = "Paul Boal"
-__email__ = "boalpe@slu.edu"
+__author__ = "Mohsen Eedloo"
+__email__ = "Mohsen.eedloo@gmail.com"
 
-#
-#TODO: Here or in other .py files, you need to add your own code to parse the JSON files
-#      This is a very simple function for parsing a much simpler JSON input file.
-#      There are many ways I could do this, but I've decided to show it using basic
-#      Python features rather than Pandas or another library because Pandads's support
-#      for JSON won't be sufficient for our real files.
-#
 def parse(inputfile, outputfile=None):
     return parser.parse(inputfile, outputfile)
 
-# This is a simple parser used for a much simpler test file
 def parse_simple(inputfile, outputfile=None):
-    """This function both writes to an output file and returns a dataframe
-       with the information. That will make testing easier."""
     data = json.load(open(inputfile))
-
-    # I know the data I'm looking for: name, type, breed
-    # Other data I might encounter, I'll ignore.
     output = []
     for item in data:
         name = item.get('name')
@@ -38,7 +25,6 @@ def parse_simple(inputfile, outputfile=None):
         df.to_csv(outputfile, index=False)
 
     return df
-
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True, dir_okay=False))
@@ -53,10 +39,6 @@ def run(filename, loglevel):
     logger.debug(f'Running in DEBUG')
     logger.info(f'Processing file: {filename}')
 
-    #
-    #TODO: Here, you need to call your functions to parse the JSON, 
-    #      create the right output format, and write that to a CSV file
-    #
     if filename[-5:] == '.json':
         outputfile = filename[:-5] + '.csv'
         df = parse(filename,outputfile)
@@ -66,10 +48,7 @@ def run(filename, loglevel):
 
     return df
 
-
-
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(funcName)s():%(lineno)d - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
     run()
-
